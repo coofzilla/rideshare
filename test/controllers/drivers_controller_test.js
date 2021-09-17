cf68535ceca9992c6679173f9e3489a1daa84bac
Jay Hernandez <jeric614@yahoo.com>
2021-09-17 22:26:38 +0900

    added driver test

--- test/controllers/drivers_controller_test.js
+++ test/controllers/drivers_controller_test.js
@@ -1,11 +1,17 @@
 const assert = require("assert");
 const request = require("supertest");
 const app = require("../../app");
+const mongoose = require("mongoose");
+
+const Driver = mongoose.model("driver");
 
 describe("Drivers controller", () => {
   it("Post to /api/drivers creates a new driver", async () => {
-    const response = await request(app).post("/api/drivers").send({
+    const count = await Driver.countDocuments();
+    await request(app).post("/api/drivers").send({
       email: "test@test.com",
     });
+    const newCount = await Driver.countDocuments();
+    assert(count + 1 === newCount);
   });
 });
